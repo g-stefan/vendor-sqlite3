e3efae943e83635cd4805d57c379039b3193d7eb
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:45:18 +0300

    Update

---
+++ build/msvc.version-dependency-update.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version-dependency-update vendor-sqlite3
+
+xyo-cc --update-version-dependency vendor-sqlite3
