f6d3cc21eb5d60a74ef9ea6dfbcc595dabfa3a3e
Grigore Stefan <g_stefan@yahoo.com>
2022-04-09 21:09:50 +0300

    Update

--- build/msvc.vendor.cmd
+++ build/msvc.vendor.cmd
@@ -23,12 +23,12 @@ del /F /Q archive\%PROJECT%-%VERSION%.7z goto:eof
 rem Source
 pushd archive
 set VENDOR=%PROJECT%-%VERSION%
-set WEB_LINK=https://www.sqlite.org/2021/sqlite-amalgamation-3360000.zip
+set WEB_LINK=https://www.sqlite.org/2022/sqlite-amalgamation-3380200.zip
 if not exist %VENDOR%.zip curl --insecure --location %WEB_LINK% --output %VENDOR%.zip
 7z x %VENDOR%.zip -aoa -o.
 del /F /Q %VENDOR%.zip
 if exist %VENDOR%.7z del /F /Q %VENDOR%.7z
-move sqlite-amalgamation-3360000 sqlite3-3.36.0
-7zr a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
+move sqlite-amalgamation-3380200 sqlite3-%VERSION%
+7z a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
 rmdir /Q /S %VENDOR%
 popd
