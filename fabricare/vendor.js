5f7ee4ca6e3d2eb5c054442d354d8c145e2b6e95
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:54:03 +0200

    Update

--- fabricare/vendor.js
+++ fabricare/vendor.js
@@ -1,6 +1,6 @@
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 // Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2022-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2022-2024 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
 messageAction("vendor");
@@ -22,22 +22,27 @@ if (Shell.hasEnv("VENDOR_SOURCE_GIT")) {
 	vendorSourceGit = Shell.getenv("VENDOR_SOURCE_GIT");
 };
 
-exitIf(Shell.system("curl --insecure --location " + vendorSourceGit + "/vendor-" + Project.name + "/releases/download/v" + Project.version + "/" + Project.vendor + ".7z --output archive/" + Project.vendor + ".7z"));
-if (Shell.getFileSize("archive/" + Project.vendor + ".7z") > 16) {
+var vendorSourceAuth = "";
+if (Shell.hasEnv("VENDOR_SOURCE_AUTH")) {
+	vendorSourceAuth = Shell.getenv("VENDOR_SOURCE_AUTH");
+};
+
+exitIf(Shell.system("curl --insecure --location " + vendorSourceGit + "/vendor-" + Project.name + "/releases/download/v" + Project.version + "/" + Project.vendor + ".7z "+vendorSourceAuth+" --output archive/" + Project.vendor + ".7z"));
+if (Shell.getFileSize("archive/" + Project.vendor + ".7z") > 32768) {
 	return;
 };
 Shell.removeFile("archive/" + Project.vendor + ".7z");
 
 // Source
 runInPath("archive", function() {
-	webLink = "https://www.sqlite.org/2023/sqlite-amalgamation-3410200.zip";
+	webLink = "https://www.sqlite.org/2023/sqlite-amalgamation-3440200.zip";
 	if (!Shell.fileExists(Project.vendor + ".zip")) {
 		exitIf(Shell.system("curl --insecure --location " + webLink + " --output " + Project.vendor + ".zip"));
 	};
 	exitIf(Shell.system("7z x " + Project.vendor + ".zip -aoa -o."));
 	Shell.removeFile(Project.vendor + ".zip");
 	Shell.removeFile(Project.vendor + ".7z");
-	Shell.rename("sqlite-amalgamation-3410200",Project.vendor);
+	Shell.rename("sqlite-amalgamation-3440200",Project.vendor);
 	exitIf(Shell.system("7z a -mx9 -mmt4 -r- -sse -w. -y -t7z " + Project.vendor + ".7z " + Project.vendor));
 	Shell.removeDirRecursively(Project.vendor);
 });
